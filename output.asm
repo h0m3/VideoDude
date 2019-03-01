ee861a2b99d896a8c1602ec81baa69ad3448411e
artur <arturpaiva.ds@gmail.com>
2019-03-01 20:44:06 -0300

    Added USART SPI drawing

--- output.asm
+++ output.asm
@@ -20,6 +20,17 @@
     nop
 .endmacro
 
+; Send Empty Char Sequency (16 cycles)
+.macro send_black
+    ldi R17, @0
+    empty_bytes:
+        sts UDR0, R0
+        wait 9
+        wait_2
+        dec R17
+    brne empty_bytes
+.endmacro
+
 setup:
     cli
     clr R0
@@ -31,6 +42,11 @@ setup:
     ; PD5 - Video Sync
     ldi R16, (1 << PORTD1) | (1 << PORTD4) | (1 << PORTD5)
     out DDRD, R16
+
+    ; NOTE: Test
+    clr R1
+    ldi R16, 255
+    mov R2, R16
 
 frame:
     ; Long Sync
@@ -72,7 +88,7 @@ frame:
     .macro overscan
         ldi R17, @0
         overscan_loop:
-            ; 4.7us HSync (75 cycles)
+            ; 4.6875us HSync (75 cycles)
             cbi PORTD, PORTD5 ; Sync Level
             wait 72
             wait_1
@@ -93,24 +109,44 @@ frame:
     ldi R24, 0
     ldi R25, 1
     active_area:
-        ; 4,7us HSync (75 cycles)
+        ; 4.6875us HSync (75 cycles)
         cbi PORTD, PORTD5 ; Sync Level
         wait 72
         wait_1
 
-        ; 5,7us back porch + 3us overscan (139 cycles)
+        ; 5,7us back porch + 3,1125us overscan (141 cycles)
         sbi PORTD, PORTD5 ; Black Level
-        wait 135
+        wait 78
         wait_2
 
-        ; 45.95us Active Area (735 cycles)
-        sbi PORTD, PORTD1 ; White
-        wait 732
-        wait_1
+        ; Setup USART SPI Mode
+        sts UBRR0H, R0
+        sts UBRR0L, R0
+        sts UCSR0A, R0
+        ldi R16, (1 << UMSEL01) | (1 << UMSEL00)
+        sts UCSR0C, R16
+        ldi R16, (1 << TXEN0)
+        sts UDR0, R0
+        sts UCSR0B, R16
+        sts UBRR0H, R0
+        sts UBRR0L, R0
+        wait 9
+        send_black 2
 
-        ; 3us overscan + 1.65us front porch (75 cycles)
-        cbi PORTD, PORTD1 ; Black Level
-        wait 69
+        ; 45us Active Area (720 cycles)
+        ldi R17, 45
+        send_stripes:
+            sts UDR0, R1
+            eor R1, R2
+            wait 9
+            wait_1
+            dec R17
+        brne send_stripes
+
+        ; 3.85us overscan + 1.65us front porch (88 cycles)
+        sts UCSR0B, R0
+        wait 81
+        wait_1
         sbiw R24, 1
     brne active_area
 
