7cabb988cfef62bc0e026473615dcf3135177536
artur <arturpaiva.ds@gmail.com>
2019-03-02 21:59:16 -0300

    Character map generator update

--- chargen.py
+++ chargen.py
@@ -1,38 +1,41 @@
 #!/usr/bin/env python
+# Generate charmap.asm from charmap.txt
+# Read charmap.txt for more information
+#
+
+from datetime import datetime
 
 def main():
     lines = []
-    lines.append([])
-    lines.append([])
-    lines.append([])
-    lines.append([])
-    lines.append([])
-    lines.append([])
-    lines.append([])
-    lines.append([])
 
     with open("charmap.txt", "r") as file:
         for textline in file.read().splitlines():
             textline = textline.strip().replace(" ", "").replace("\t", "")
-            if len(textline) == 16 and not(textline.startswith("#")):
-                lines[7].append(textline[:2])
-                lines[6].append(textline[2:4])
-                lines[5].append(textline[4:6])
-                lines[4].append(textline[6:8])
-                lines[3].append(textline[8:10])
-                lines[2].append(textline[10:12])
-                lines[1].append(textline[12:14])
-                lines[0].append(textline[14:])
+            if len(textline) == 16 and not textline.startswith("#"):
+                for line in range(8):
+                    if len(lines) <= line:
+                        lines.append([])
+                    lines[line].append(textline[line * 2:line * 2 + 2])
 
-    newfile = "; Automatically generated file, use charmap.txt and chargen.py\n\n\n.org 0x0400\n\ncharmap:"
+    newfile = "; Automatically generated file based on charmap.txt\n"
+    newfile += "; See charmap.txt and chargen.py for more information\n"
+    newfile += "; Generated %s\n" % datetime.now()
+    newfile += ";\n"
+    newfile += "\n.org 0x400\n"
+    newfile += "charmap:\n"
 
     for line in range(8):
         newfile += "\n; Line %d\n" % line
         for idx, item in enumerate(lines[line]):
             if idx % 2:
-                newfile += ".dw 0x%s%s ; 0x%02x and 0x%02x\n" % (item, lines[line][idx - 1], idx, idx - 1)
+                newfile += ".dw 0x%s%s ; 0x%02x and 0x%02x\n" % (
+                    item,
+                    lines[line][idx - 1],
+                    idx,
+                    idx - 1
+                )
 
-    with open ("charmap.asm", "w") as file:
+    with open("charmap.asm", "w") as file:
         file.write(newfile)
 
 if __name__ == "__main__":
